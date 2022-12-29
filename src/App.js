5b60baf2e369a1bb5f677f6ea4ac4dc1251f6876
JunjPan <elisepjj@hotmail.com>
2022-12-28 16:09:05 -0800

    add s3 bucket function

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { useEffect, useState } from 'react';
 import { API, graphqlOperation } from 'aws-amplify';
+import S3FileUpload from 'react-s3';
 import { createTodo, deleteTodo } from './graphql/mutations';
 import { listTodos } from './graphql/queries';
 import { onDeleteTodo } from './graphql/subscriptions';
+window.Buffer=window.Buffer||require('buffer').Buffer;
 
 const initialState = { name: '', description: '' };
+const S3_BUCKET =process.env.REACT_APP_S3_BUCKET;
+const REGION =process.env.REACT_APP_REGION;
+const ACCESS_KEY =process.env.REACT_APP_ACCESS_KEY;
+const SECRET_ACCESS_KEY =process.env.REACT_APP_SECRET_ACCESS_KEY;
+
+
+
+const config = {
+    bucketName: S3_BUCKET,
+    region: REGION,
+    accessKeyId: ACCESS_KEY,
+    secretAccessKey: SECRET_ACCESS_KEY,
+}
 
 const App = () => {
   const [formState, setFormState] = useState(initialState);
   const [todos, setTodos] = useState([]);
+  const [selectedFile, setSelectedFile] = useState(null);
+
 
   useEffect(() => {
   
@@ -53,7 +70,20 @@ const App = () => {
     }
   };
 
+  const handleFileInput = (e) => {
+    setSelectedFile(e.target.files[0]);
+    console.log(e.target.files[0])
+}
+
+const handleUpload = async (file) => {
+  console.log(config)
+  S3FileUpload.uploadFile(file, config)
+        .then(data => console.log(data))
+        .catch(err => console.error(err))
+}
+
   return (
+    <>
     <div style={styles.container}>
       <h2>Amplify Todos</h2>
       <input
@@ -84,6 +114,11 @@ const App = () => {
         </div>
       ))}
     </div>
+    <div>
+        <div>React S3 File Upload</div>
+        <input type="file" onChange={handleFileInput}/>
+        <button onClick={() => handleUpload(selectedFile)}> Upload to S3</button>
+    </div></>
   );
 };
 
